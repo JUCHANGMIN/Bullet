0aaaed6559946eeddacee820fc97eaca8a037aeb
Joft1990 <changmin90@me.com>
2015-06-09 15:26:21 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package BulletProject;
+
+/**
+ * Created by Joft on 2015-06-09.
+ */
+
+public class Main{
+
+    public static void main(String[] ar){
+
+        StartFrame sfm = new StartFrame();
+    }
+}
